96db2bbfc37a65da7531f334115ba56a1afff396
Martín Betancur <mbetancur@q10.com>
2026-04-16 11:15:43 -0500

    Se comienza con la creación de página de compra de bitcoins

---
+++ finance-dashboard/src/components/TransactionHistory/TransactionHistory.tsx
@@ -0,0 +1,14 @@
+export function TransactionHistory() {
+    return (
+        <div className="transaction-history">
+            <h4>Historial de Movimientos</h4>
+            <input type="text" placeholder="Buscar por categoría..." />
+            <button>Limpiar Historial</button>
+
+            <ul>
+                <li>
+                </li>
+            </ul>
+        </div>
+    );
+}
